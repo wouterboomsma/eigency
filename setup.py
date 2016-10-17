bfeef5698538da960c4e492315e8f2da3ae0c82a
Phil Austin <paustin@eos.ubc.ca>
2016-10-17 14:05:52 -0700

    move setup to setuptools

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 import os
-from distutils import file_util
-from distutils import dir_util
-from distutils import sysconfig
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup
+from distutils import dir_util, file_util
+from setuptools.extension import Extension
 from Cython.Build import cythonize
+import glob
 
 import eigency
 import numpy as np
@@ -29,6 +28,18 @@ try:
 except(IOError, ImportError):
     long_description = open('README.md').read()
 
+datafiles = [(d, [os.path.join(d,f) for f in files])
+    for d, folders, files in os.walk('eigency/eigen_3.2.8')]
+
+datafiles2 = []
+os.chdir('eigency')
+for d, folders, files in  os.walk('eigen_3.2.8'):
+    the_files=[os.path.join(d,f) for f in files]
+    datafiles2.extend(the_files)
+os.chdir('..')
+
+print(datafiles2[:40])    
+    
 dist = setup(
     name = __package_name__,
     version = "1.70",
@@ -38,25 +49,28 @@ dist = setup(
     author_email = "wb@bio.ku.dk",
     url = "https://github.com/wouterboomsma/eigency",
     download_url = "https://github.com/wouterboomsma/eigency/tarball/1.70",
+    include_package_data = True,
+    package_data={'eigency':datafiles2},
     ext_modules = cythonize(extensions),
+    data_files=datafiles,
     packages = [__package_name__]
 )
 
-if 'install' in dist.command_obj:
-    destination_path = dist.command_obj['install'].install_lib
-    package_path = os.path.join(destination_path, __package_name__)
+# if 'install' in dist.command_obj:
+#     destination_path = dist.command_obj['install'].install_lib
+#     package_path = os.path.join(destination_path, __package_name__)
+#     # Copy cython files
+#     file_util.copy_file('eigency/core.pxd', package_path, update=1, preserve_mode=0)
+#     file_util.copy_file('eigency/core.pyx', package_path, update=1, preserve_mode=0)
+#     file_util.copy_file('eigency/conversions.pxd', package_path, update=1, preserve_mode=0)
+#     file_util.copy_file('eigency/conversions.pyx', package_path, update=1, preserve_mode=0)
 
-    # Copy cython files
-    file_util.copy_file('eigency/core.pxd', package_path, update=1, preserve_mode=0)
-    file_util.copy_file('eigency/core.pyx', package_path, update=1, preserve_mode=0)
-    file_util.copy_file('eigency/conversions.pxd', package_path, update=1, preserve_mode=0)
-    file_util.copy_file('eigency/conversions.pyx', package_path, update=1, preserve_mode=0)
-
-    # Copy C++ file
-    file_util.copy_file('eigency/eigency_cpp.h', package_path, update=1, preserve_mode=0)
-    file_util.copy_file('eigency/conversions_api.h', package_path, update=1, preserve_mode=0)
+#     # Copy C++ file
+#     file_util.copy_file('eigency/eigency_cpp.h', package_path, update=1, preserve_mode=0)
+#     file_util.copy_file('eigency/conversions_api.h', package_path, update=1, preserve_mode=0)
     
-    # Copy Eigen header files
-    eigen_path = os.path.join(package_path, __eigen_dir__)
-    dir_util.copy_tree(__eigen_dir__, eigen_path, update=1, preserve_mode=0)
+#     # Copy Eigen header files
+#     eigen_path = os.path.join(package_path, __eigen_dir__)
+#     dir_util.copy_tree(__eigen_dir__, eigen_path, update=1, preserve_mode=0)
 
+#https://en.wikipedia.org/wiki/Inverse_trigonometric_functions
--- tests/setup.py
+++ tests/setup.py
@@ -1,7 +1,7 @@
 import os
 from distutils import file_util
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup
+from setuptools.extension import Extension
 from Cython.Build import cythonize
 
 import eigency
