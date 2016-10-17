48363728fb9517d53cd066d9a2ff408f3b8fc300
Phil Austin <paustin@eos.ubc.ca>
2016-10-17 15:24:04 -0700

    move run_tests to module

--- setup.py
+++ setup.py
@@ -51,21 +51,3 @@ dist = setup(
     packages = [__package_name__]
 )
 
-# if 'install' in dist.command_obj:
-#     destination_path = dist.command_obj['install'].install_lib
-#     package_path = os.path.join(destination_path, __package_name__)
-#     # Copy cython files
-#     file_util.copy_file('eigency/core.pxd', package_path, update=1, preserve_mode=0)
-#     file_util.copy_file('eigency/core.pyx', package_path, update=1, preserve_mode=0)
-#     file_util.copy_file('eigency/conversions.pxd', package_path, update=1, preserve_mode=0)
-#     file_util.copy_file('eigency/conversions.pyx', package_path, update=1, preserve_mode=0)
-
-#     # Copy C++ file
-#     file_util.copy_file('eigency/eigency_cpp.h', package_path, update=1, preserve_mode=0)
-#     file_util.copy_file('eigency/conversions_api.h', package_path, update=1, preserve_mode=0)
-    
-#     # Copy Eigen header files
-#     eigen_path = os.path.join(package_path, __eigen_dir__)
-#     dir_util.copy_tree(__eigen_dir__, eigen_path, update=1, preserve_mode=0)
-
-#https://en.wikipedia.org/wiki/Inverse_trigonometric_functions
