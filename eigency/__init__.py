5978e27f95bc4564c08b0be2cba23d6efa9b6095
hwied <hwiedemann@gmx.net>
2020-07-21 16:33:32 +0200

    Remove cmake

--- eigency/__init__.py
+++ eigency/__init__.py
@@ -1,8 +1,9 @@
 from pkg_resources import resource_filename
 import numpy as np
 import os.path
+import pkgconfig
 
-__eigen_dir__ = "@EIGEN_REL_PATH@"
+__eigen_dir__ = os.path.relpath(pkgconfig.cflags('eigen3')[2:])
 
 def get_includes(include_eigen=True):
     root = os.path.dirname(__file__)
