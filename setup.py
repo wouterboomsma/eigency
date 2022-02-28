2cf0d7f3a1249d0c925aaf89e365e255932d71a2
Bradley Odell <btodell@hotmail.com>
2022-02-27 22:45:59 -0800

    Restrict version of build deps

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ dist = setup(
     author_email = "wb@di.ku.dk",
     url = "https://github.com/wouterboomsma/eigency",
     use_scm_version = True,
-    setup_requires = ['setuptools>=38','setuptools_scm'],
+    setup_requires = ['setuptools>=38.6.0','setuptools_scm==5.0.2'],
     ext_modules = extensions,
     packages = find_packages(),
     include_package_data=True,
