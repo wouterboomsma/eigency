b6b0049f33ca2259e45379e88037a76e62d8c72f
Bradley Odell <btodell@hotmail.com>
2022-03-05 14:19:19 -0800

    Fixing typedefs.

--- eigency/eigency_typedefs.h
+++ eigency/eigency_typedefs.h
@@ -18,10 +18,10 @@ typedef ::Eigen::Matrix<Type, 1, Size>    RowVector##SizeSuffix##TypeSuffix;
 EIGENCY_MAKE_TYPEDEFS(Type, TypeSuffix, 2, 2)               \
 EIGENCY_MAKE_TYPEDEFS(Type, TypeSuffix, 3, 3)               \
 EIGENCY_MAKE_TYPEDEFS(Type, TypeSuffix, 4, 4)               \
-EIGENCY_MAKE_TYPEDEFS(Type, TypeSuffix, Dynamic, X)
+EIGENCY_MAKE_TYPEDEFS(Type, TypeSuffix, ::Eigen::Dynamic, X)
 
-EIGENCY_MAKE_TYPEDEFS_ALL_SIZES(long double,               ld)
-EIGENCY_MAKE_TYPEDEFS_ALL_SIZES(std::complex<long double>, cld)
+EIGENCY_MAKE_TYPEDEFS_ALL_SIZES(long double,                 ld)
+EIGENCY_MAKE_TYPEDEFS_ALL_SIZES(::std::complex<long double>, cld)
 
 #undef EIGENCY_MAKE_TYPEDEFS_ALL_SIZES
 #undef EIGENCY_MAKE_TYPEDEFS
@@ -34,21 +34,21 @@ EIGENCY_MAKE_TYPEDEFS_ALL_SIZES(std::complex<long double>, cld)
 typedef ::Eigen::Array<Type, Size, Size> Array##SizeSuffix##SizeSuffix##TypeSuffix; \
 typedef ::Eigen::Array<Type, Size, 1>    Array##SizeSuffix##TypeSuffix;
 
-#define EIGENCY_MAKE_ARRAY_FIXED_TYPEDEFS(Type, TypeSuffix, Size)       \
-typedef ::Eigen::Array<Type, Size, Dynamic> Array##Size##X##TypeSuffix; \
-typedef ::Eigen::Array<Type, Dynamic, Size> Array##X##Size##TypeSuffix;
+#define EIGENCY_MAKE_ARRAY_FIXED_TYPEDEFS(Type, TypeSuffix, Size)                   \
+typedef ::Eigen::Array<Type, Size, ::Eigen::Dynamic> Array##Size##X##TypeSuffix;    \
+typedef ::Eigen::Array<Type, ::Eigen::Dynamic, Size> Array##X##Size##TypeSuffix;
 
-#define EIGENCY_MAKE_ARRAY_TYPEDEFS_ALL_SIZES(Type, TypeSuffix) \
-EIGENCY_MAKE_ARRAY_TYPEDEFS(Type, TypeSuffix, 2, 2)             \
-EIGENCY_MAKE_ARRAY_TYPEDEFS(Type, TypeSuffix, 3, 3)             \
-EIGENCY_MAKE_ARRAY_TYPEDEFS(Type, TypeSuffix, 4, 4)             \
-EIGENCY_MAKE_ARRAY_TYPEDEFS(Type, TypeSuffix, Dynamic, X)       \
-EIGENCY_MAKE_ARRAY_FIXED_TYPEDEFS(Type, TypeSuffix, 2)          \
-EIGENCY_MAKE_ARRAY_FIXED_TYPEDEFS(Type, TypeSuffix, 3)          \
+#define EIGENCY_MAKE_ARRAY_TYPEDEFS_ALL_SIZES(Type, TypeSuffix)     \
+EIGENCY_MAKE_ARRAY_TYPEDEFS(Type, TypeSuffix, 2, 2)                 \
+EIGENCY_MAKE_ARRAY_TYPEDEFS(Type, TypeSuffix, 3, 3)                 \
+EIGENCY_MAKE_ARRAY_TYPEDEFS(Type, TypeSuffix, 4, 4)                 \
+EIGENCY_MAKE_ARRAY_TYPEDEFS(Type, TypeSuffix, ::Eigen::Dynamic, X)  \
+EIGENCY_MAKE_ARRAY_FIXED_TYPEDEFS(Type, TypeSuffix, 2)              \
+EIGENCY_MAKE_ARRAY_FIXED_TYPEDEFS(Type, TypeSuffix, 3)              \
 EIGENCY_MAKE_ARRAY_FIXED_TYPEDEFS(Type, TypeSuffix, 4)
 
-EIGENCY_MAKE_ARRAY_TYPEDEFS_ALL_SIZES(long double,               ld)
-EIGENCY_MAKE_ARRAY_TYPEDEFS_ALL_SIZES(std::complex<long double>, cld)
+EIGENCY_MAKE_ARRAY_TYPEDEFS_ALL_SIZES(long double,                 ld)
+EIGENCY_MAKE_ARRAY_TYPEDEFS_ALL_SIZES(::std::complex<long double>, cld)
 
 #undef EIGENCY_MAKE_ARRAY_TYPEDEFS_ALL_SIZES
 #undef EIGENCY_MAKE_ARRAY_FIXED_TYPEDEFS
