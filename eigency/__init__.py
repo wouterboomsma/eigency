1294baad4c31a6248853e62a003a7bc9972534ff
BORDEU Felipe <felipe.bordeu@safrangroup.com>
2022-04-07 14:15:44 +0200

    User can use system eigen

--- eigency/__init__.py
+++ eigency/__init__.py
@@ -1,14 +1,17 @@
 import os.path
 
 import numpy as np
-from pkg_resources import resource_filename
 
-__eigen_dir__ = resource_filename(__name__, "eigen")
+
+def get_eigency_eigen_dir():
+    from pkg_resources import resource_filename
+    return resource_filename(__name__, "eigen")
 
 
 def get_includes(include_eigen=True):
     root = os.path.dirname(__file__)
     path = [root, np.get_include()]
     if include_eigen:
+        __eigen_dir__ = get_eigency_eigen_dir()
         path.append(os.path.join(root, __eigen_dir__))
     return path
