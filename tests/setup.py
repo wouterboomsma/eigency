baa8fe5abf4faa6ce080aa18c7c3e9cd730917b5
Bradley Odell <btodell@hotmail.com>
2023-03-25 23:17:22 -0700

    Update Eigen to version 3.4.0 (#61)
    - Update Eigen to version 3.4.0
    - Update tool versions
    - Update GitHub Actions workflows
    - Fix setuptools deprecation warning
    - Remove Python 3.5 and 3.6 support (EOL).
    - Disable MacOS testing (unknown cause of failure)
    
    Closes #60

--- tests/setup.py
+++ tests/setup.py
@@ -13,8 +13,13 @@ extensions = [
 ]
 
 setup(
-    name="eigency_tests",
+    name="eigency-tests",
     version="0.0.0",
-    ext_modules=cythonize(extensions),
+    ext_modules=cythonize(
+        extensions,
+        compiler_directives=dict(
+            language_level="3",
+        ),
+    ),
     packages=["eigency_tests"],
 )
